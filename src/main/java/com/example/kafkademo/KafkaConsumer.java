ec99b48dcb77cd80cb427bdc1ac5ccf16e1b955a
Clayton Phillips <claytos_aus@yahoo.com>
2021-07-09 20:55:04 +1000

    use avro class

--- src/main/java/com/example/kafkademo/KafkaConsumer.java
+++ src/main/java/com/example/kafkademo/KafkaConsumer.java
@@ -1,24 +1,18 @@
 package com.example.kafkademo;
 
-import java.util.Map;
-
-import org.springframework.context.annotation.Bean;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.converter.RecordMessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-@Component
-public class KafkaConsumer {
+import example.avro.User;
+import lombok.extern.slf4j.Slf4j;
 
-    @Bean
-	public RecordMessageConverter converter() {
-		return new StringJsonMessageConverter();
-	}
+@Component
+@Slf4j
+public class KafkaConsumer {
     
     @KafkaListener(topics = "${kafka.topic}", groupId = "consumer-group")
-    public void consume(@Payload Map<String,Object> m) {
-        System.out.println("received:" + m.toString());
+    public void consume(ConsumerRecord<String,User> m) {
+        log.info("received:" + m.value().toString());
     }
 }
--- src/main/java/com/example/kafkademo/StartupRunner.java
+++ src/main/java/com/example/kafkademo/StartupRunner.java
@@ -3,19 +3,24 @@ package com.example.kafkademo;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.SuccessCallback;
 
 import example.avro.User;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class StartupRunner implements CommandLineRunner {
 	
 	@Autowired
-	private KafkaTemplate<String,Object> kafkaTemplate;
+	private KafkaTemplate<String,User> kafkaTemplate;
 
 	User user;
 
@@ -24,10 +29,12 @@ public class StartupRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Map<String,String> m = new HashMap<String,String>();
-		m.put("a","1");
-		kafkaTemplate.send(topic,m);
-		System.out.println("published data...");
+		// Map<String,String> m = new HashMap<String,String>();
+		// m.put("a","1");
+		user = new User("test user", 3, "blue");
+		kafkaTemplate.send(topic,user).addCallback(
+			result -> log.info("sent data"), 
+			exception -> log.error("error occurred"));
 	}
 
 }
